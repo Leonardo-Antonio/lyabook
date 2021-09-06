e0f22426540f67ac23003072a552bd8fd520cac1
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-09-06 02:48:06 -0500

    externa de libros

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,127 +1,131 @@
 <template>
   <div>
     <div class="mt-20">
-      <el-container class="flex justify-center w-7/12 mx-auto">
-        <div class="container-filter">
-          <div class="header-filter px-6 py-4 rounded-t-2xl">
-            <p>Filtar por</p>
-          </div>
-          <div>
-            <el-row class="tac">
-              <el-col :span="12" class="column-menu">
-                <el-menu
-                  default-active="2"
-                  class="el-menu-vertical-demo header-option-filter"
-                  @open="handleOpen"
-                  @close="handleClose"
-                >
-                  <el-submenu index="1">
-                    <template slot="title">
-                      <!-- <i class="el-icon-location"></i> -->
-                      <span class="subtitle-filter">Categoria</span>
-                    </template>
-                    <el-menu-item index="1-1"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-2"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-3"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-4"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                  </el-submenu>
-                </el-menu>
-                <el-menu
-                  default-active="2"
-                  class="el-menu-vertical-demo header-option-filter"
-                  @open="handleOpen"
-                  @close="handleClose"
-                >
-                  <el-submenu index="2">
-                    <template slot="title">
-                      <!-- <i class="el-icon-location"></i> -->
-                      <span class="subtitle-filter">Editorial</span>
-                    </template>
-                    <el-menu-item index="1-1"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-2"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-3"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-4"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                  </el-submenu>
-                </el-menu>
-                <el-menu
-                  default-active="2"
-                  class="el-menu-vertical-demo header-option-filter"
-                  @open="handleOpen"
-                  @close="handleClose"
-                >
-                  <el-submenu index="3">
-                    <template slot="title">
-                      <!-- <i class="el-icon-location"></i> -->
-                      <span class="subtitle-filter">Autor</span>
-                    </template>
-                    <el-menu-item index="1-1"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-2"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-3"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                    <el-menu-item index="1-4"
-                      ><el-checkbox v-model="checked"
-                        >Opción</el-checkbox
-                      ></el-menu-item
-                    >
-                  </el-submenu>
-                </el-menu>
-                <el-menu>
-                  <div class="container-barra-price pt-4">
-                    <div class="block">
-                      <span class="demonstration subtitle-filter">Precio</span>
-                      <el-slider
-                        class="pt-2 px-2"
-                        v-model="value_barra"
-                      ></el-slider>
-                    </div>
-                  </div>
-                </el-menu>
-              </el-col>
-            </el-row>
-          </div>
+      <div class="flex justify-center w-7/12 mx-auto">
+        <div class="container-filter-father">
+            <!-- ssss -->
+            <div class="container-filter">
+                <div class="header-filter px-6 py-4 rounded-t-2xl">
+                    <p class="header-title">Filtar por</p>
+                </div>
+                <div>
+                    <el-row class="tac">
+                    <el-col :span="12" class="column-menu">
+                        <el-menu
+                        default-active="2"
+                        class="el-menu-vertical-demo header-option-filter"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        >
+                        <el-submenu index="1">
+                            <template slot="title">
+                            <!-- <i class="el-icon-location"></i> -->
+                            <span class="subtitle-filter">Categoria</span>
+                            </template>
+                            <el-menu-item index="1-1"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-2"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-3"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-4"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                        </el-submenu>
+                        </el-menu>
+                        <el-menu
+                        default-active="2"
+                        class="el-menu-vertical-demo header-option-filter"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        >
+                        <el-submenu index="2">
+                            <template slot="title">
+                            <!-- <i class="el-icon-location"></i> -->
+                            <span class="subtitle-filter">Editorial</span>
+                            </template>
+                            <el-menu-item index="1-1"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-2"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-3"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-4"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                        </el-submenu>
+                        </el-menu>
+                        <el-menu
+                        default-active="2"
+                        class="el-menu-vertical-demo header-option-filter"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        >
+                        <el-submenu index="3">
+                            <template slot="title">
+                            <!-- <i class="el-icon-location"></i> -->
+                            <span class="subtitle-filter">Autor</span>
+                            </template>
+                            <el-menu-item index="1-1"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-2"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-3"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                            <el-menu-item index="1-4"
+                            ><el-checkbox v-model="checked"
+                                >Opción</el-checkbox
+                            ></el-menu-item
+                            >
+                        </el-submenu>
+                        </el-menu>
+                        <el-menu class="rounded-b-2xl"> 
+                            <div class="container-barra-price pt-4 pb-20">
+                                <div class="block">
+                                <span class="demonstration subtitle-filter">Precio</span>
+                                <el-slider
+                                    class="pt-2 px-2"
+                                    v-model="value_barra"
+                                ></el-slider>
+                                </div>
+                            </div>
+                        </el-menu>
+                    </el-col>
+                    </el-row>
+                </div>
+            </div>
+            <!-- ssss -->
         </div>
 
         <!-- 2da barra -->
@@ -151,13 +155,13 @@
               <div class="w-full">
                 <div>
                   <div class="flex flex-wrap">
-                    <div v-for="item of [2, 3, 4, 5, 6, 7, 8]" :key="item" class="pl-4 pb-4">
+                    <div v-for="item of [2, 3, 4, 5, 6, 7, 8]" :key="item" class="pl-4 pb-8">
                       <img
                         class="payment-card"
                         src="/images/example-product.png"
                       />
-                      <p class="title-product ">Memory - Lyabook</p>
-                      <div class="flex flex justify-center items-center pt-2">
+                      <p class="title-product pt-2">Memory - Lyabook</p>
+                      <div class="flex justify-center items-center pt-2">
                         <p class="w-3/6	price-before">S/ 249.9</p>
                         <p class="w-3/6	price-current">S/ 149.9</p>
                       </div>
@@ -182,10 +186,19 @@
               </div>
             </div>
           </el-main>
-          <el-footer>Pie de página</el-footer>
+          <el-footer>
+              <div class="flex justify-center">
+                  <div class="block">
+                    <el-pagination
+                        layout="prev, pager, next"
+                        :total="1000">
+                    </el-pagination>
+                  </div>
+              </div>
+          </el-footer>
         </el-container>
         <!-- fin de la barra 2 -->
-      </el-container>
+      </div>
     </div>
   </div>
 </template>
@@ -241,10 +254,16 @@ export default {
 }
 .column-menu {
   width: 100% !important;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+.container-filter-father{
+    width: 37%;
 }
 .container-filter {
   box-shadow: 2px 4px 10px rgba(49, 64, 95, 0.15);
   border-radius: 10px;
+  
 }
 .container-title {
   font-family: Qucksand;
@@ -264,9 +283,6 @@ export default {
 .container-barra-price {
   padding-left: 1.2rem;
   padding-right: 1.2rem;
-}
-.container-filter{
-    width: 37%;
 }
 .header-list-book{
     padding-left: 2.5rem !important;
@@ -294,5 +310,15 @@ export default {
 }
 .container-barra-price .el-slider__bar{
     background-color: #021639;
+}
+.header-title{
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+li.el-submenu {
+    border-bottom: 1px solid #D5D5D5;
+}
+.el-menu {
+    border-right: unset;
 }
 </style>
