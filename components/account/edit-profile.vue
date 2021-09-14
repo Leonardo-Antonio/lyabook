9d060b5e2e7c68c40e5caacae9b96ba2f86441a9
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-09-14 03:09:06 -0500

    finish-edit-profile

---
+++ components/account/edit-profile.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div class="container-perfile py-12 px-8 m-4 w-3/5">
+      <p class="titulo-form-book">Nombre</p>
+      <el-input
+        class="input mt-2"
+        placeholder="Nombre"
+        v-model="name"
+        clearable
+      >
+      </el-input>
+
+      <p class="titulo-form-book pt-4">Apellido</p>
+      <el-input
+        class="input mt-2"
+        placeholder="Apellido"
+        v-model="apellido"
+        clearable
+      >
+      </el-input>
+
+      <p class="titulo-form-book pt-4">Documento de Identidad</p>
+      <el-input
+        class="input mt-2"
+        placeholder="Documento de Identidad"
+        v-model="dni"
+        clearable
+      >
+      </el-input>
+
+      <p class="titulo-form-book pt-4">Teléfono personal</p>
+      <el-input
+        class="input mt-2"
+        placeholder="Teléfono personal"
+        v-model="telefono"
+        clearable
+      >
+      </el-input>
+
+      <p class="titulo-form-book pt-4">Género</p>
+      <el-select
+        v-model="value_gender"
+        clearable
+        placeholder="Género"
+        class="mt-2 select-gender"
+      >
+        <el-option
+          v-for="item in gender"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
+
+      <p class="titulo-form-book pt-4">Fecha de nacimiento</p>
+      <el-input
+        class="input mt-2"
+        placeholder="Fecha de nacimiento"
+        v-model="date"
+        clearable
+      >
+      </el-input>
+
+      <div class="flex justify-center pt-8">
+        <el-button
+          class="btn_save_change"
+          type="primary"
+          >Guardar Cambios</el-button
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      gender: [
+        {
+          value: 'Femenino',
+          label: 'Femenino',
+        },
+        {
+          value: 'Masculino',
+          label: 'Masculino',
+        },
+        {
+          value: 'Otro',
+          label: 'Otro',
+        },
+      ],
+      value_gender: '',
+    }
+  },
+}
+</script>
+
+<style scoped>
+.btn_save_change{
+    width: 100%;
+    height: 4rem;
+    border-radius: 5rem;
+    background: #021639;
+    border-color: #021639;
+}
+.btn_save_change:hover{
+    transform: unset
+}
+</style>
---
+++ components/account/profile.vue
@@ -0,0 +1,138 @@
+<template>
+  <div>
+    <div v-show="showEdit">
+      <div class="ml-4">
+        <p class="title-account pb-2">Perfil</p>
+      </div>
+      <div class="flex">
+        <div class="container-perfile p-12 m-4 w-3/5">
+          <div class="flex">
+            <div class="cp-1">
+              <div>
+                <p class="text-title">Nombre</p>
+                <p class="text-p mt-2">Pedro</p>
+              </div>
+              <div class="mt-8">
+                <p class="text-title">Email</p>
+                <p class="text-p mt-2">pedrolopez@gmail.com</p>
+              </div>
+              <div class="mt-8">
+                <p class="text-title">DNI</p>
+                <p class="text-p mt-2">12345678</p>
+              </div>
+              <div class="mt-8">
+                <p class="text-title">Fecha de nacimiento</p>
+                <p class="text-p mt-2">12 de enero 1996</p>
+              </div>
+            </div>
+            <div class="cp-2">
+              <div>
+                <p class="text-title">Apellido</p>
+                <p class="text-p mt-2">Lopez</p>
+              </div>
+              <div class="space"></div>
+              <div class="mt-8">
+                <p class="text-title">Género</p>
+                <p class="text-p mt-2">Masculino</p>
+              </div>
+              <div class="mt-8">
+                <p class="text-title">Celular</p>
+                <p class="text-p mt-2">999 999 999</p>
+              </div>
+            </div>
+          </div>
+          <div class="container-button-edit">
+            <el-button
+              class="btn_add_size button-to-by btn-edit"
+              type="primary"
+              round
+              @click="showEdit = false"
+              >Editar</el-button
+            >
+          </div>
+        </div>
+
+        <div class="w-2/5">
+          <div class="container-password p-12 m-4">
+            <p class="text-title">Contraseña</p>
+            <p class="text-p mt-2">
+              Usted todavía no tiene una contraseña definida
+            </p>
+            <div class="container-btn-password flex justify-end">
+              <el-button
+                class="btn_add_size button-to-by btn-password"
+                type="primary"
+                round
+                >Definir Contraseña</el-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-show="!showEdit">
+      <div class="flex items-center ml-4 container-return">
+        <el-button @click="showEdit = true">
+          <box-icon
+            name="arrow-back"
+            animation="flashing"
+            color="#021639"
+          ></box-icon>
+        </el-button>
+
+        <el-button
+          class="return-button"
+          type="primary"
+          round
+          @click="showEdit = true"
+          >Regresar</el-button
+        >
+      </div>
+      <div class="ml-4 pt-4">
+        <p class="title-account pb-2">Editar Perfil</p>
+      </div>
+      <div>
+          <EditProfile/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import EditProfile from '../account/edit-profile'
+export default {
+  components: {
+    EditProfile
+  },
+  data() {
+    return {
+      showEdit: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.return-button {
+  padding: 0px !important;
+  margin-left: 0.5rem !important;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 21px;
+  color: #021639;
+  background: unset;
+  border-color: #fff;
+}
+
+.container-return .el-button {
+  padding: 0;
+  border-color: #fff;
+}
+
+.container-return .el-button:hover {
+  background: unset;
+}
+</style>
