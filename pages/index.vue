de0261f8b1d4091e8b3ccf05161d8c8a0f3419be
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-09-16 02:18:44 -0500

    banner-1 home  finish

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    <el-input placeholder="Please input" v-model="input" clearable> </el-input>
-    <button @click="add">add</button>
+    <div class="flex justify-center">
+      <Banner1 />
+    </div>
+    
   </div>
 </template>
 
 <script>
+import Banner1 from '../components/home/baner-1'
 export default {
+  components: {
+    Banner1
+  },
   data() {
     return {
       input: '',
     }
   },
   methods: {
-    async add() {
-      const data = await this.$axios.$get('/todos/1')
-      console.log(data)
-    }
-  }
+    // async add() {
+    //   const data = await this.$axios.$get('/todos/1')
+    //   console.log(data)
+    // },
+  },
 }
 </script>
+<style scoped></style>
