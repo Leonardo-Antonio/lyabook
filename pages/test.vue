8d82b01cfed6cb4f8c8d33fbef1b2debf0c4993a
Leonardo Antonio Nolasco Leyva <leo@pop-os.localdomain>
2021-10-10 02:34:33 -0500

    example map

--- pages/test.vue
+++ pages/test.vue
@@ -1,69 +1,19 @@
 <template>
-  <div>
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-      <el-radio-button :label="false">expand</el-radio-button>
-      <el-radio-button :label="true">collapse</el-radio-button>
-    </el-radio-group>
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      @open="handleOpen"
-      @close="handleClose"
-      :collapse="isCollapse"
-    >
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">Navigator One</span>
-        </template>
-        <el-menu-item-group>
-          <span slot="title">Group One</span>
-          <nuxt-link to="/login"
-            ><el-menu-item index="1-1">item one</el-menu-item></nuxt-link
-          >
-          <el-menu-item index="1-2">item two</el-menu-item>
-        </el-menu-item-group>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">Navigator Two</span>
-      </el-menu-item>
-      <nuxt-link to="/"
-        ><el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">Navigator Four</span>
-        </el-menu-item></nuxt-link
-      >
-    </el-menu>
-  </div>
+  <CargarMapa
+    :configMapa="configMapa"
+    apiKey="TU API KEY"
+  >
+ 
+    <template slot-scope="{ google, map }">
+      <Marcadores
+        v-for="marker in markers"
+        :key="marker.id"
+        :marker="marker"
+        :google="google"
+        :map="map"
+      />
+    </template>
+ 
+  </CargarMapa>
+ 
 </template>
-
-<style>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 400px;
-  min-height: 100vh;
-  overflow: hidden;
-}
-
-.el-menu-vertical-demo {
-    overflow: hidden;
-}
-</style>
-
-<script>
-export default {
-  data() {
-    return {
-      isCollapse: true,
-    }
-  },
-  methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath)
-    },
-  },
-}
-</script>
