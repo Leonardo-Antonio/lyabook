940b4db18a0115c4836bb02cf2565538503b8375
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-11-25 00:05:52 -0500

    add margin footer

--- components/layouts/footer.vue
+++ components/layouts/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="r-remove r-remove-tablet mt-12">
+    <div class="r-remove r-remove-tablet">
       <div class="footer flex justify-center px-8">
         <div class="conatiner-footer flex flex-row justify-center py-8" style="width: 80%;">
           <div class="container-column-1 w-1/5 mx-4">
@@ -110,7 +110,7 @@
     </div>
 
     <div class="r-see r-remove-desktop container-footer-father">
-      <el-collapse accordion class="r-px-2 mt-12 footer-container-response">
+      <el-collapse accordion class="r-px-2 footer-container-response">
         <el-collapse-item title="Contacto" name="1">
           <div>
             <ul>
@@ -478,9 +478,7 @@ export default {
   }
 }
 
-.footer-container-response{
-  margin-top: 3rem !important;
-}
+
 
 
 </style>
